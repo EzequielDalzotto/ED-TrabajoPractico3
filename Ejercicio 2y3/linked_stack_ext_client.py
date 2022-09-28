159d4a84476123d8a6092843cd6167dd3f36362e
EzequielDalzotto <dalzottoezequiel@gmail.com>
2022-09-28 00:05:12 -0300

    Demostracion de funcionamiento mas limpia

--- Ejercicio 2y3/linked_stack_ext_client.py
+++ Ejercicio 2y3/linked_stack_ext_client.py
@@ -1,5 +1,5 @@
+from util import h1, h2
 from linked_stack_ext import LinkedStackExt
-# from linked_stack import LinkedStack
 
 pila = LinkedStackExt()
 
@@ -7,15 +7,25 @@ pila.push(12)
 pila.push(14)
 pila.push(17)
 pila.push(40)
+pila.push(50)
+pila.push(22)
+pila.push(17)
+pila.push(19)
+pila.push(17)
+
+h1("Impresion de la pila")
 print(pila)
 
-# for i in pila.multi_pop(2):
-#     print(i)
+h2("Multi pop:")
+for i in pila.multi_pop(2):
+    print(i)
 
-# pila.replace_all(17,4)
-# print(pila)
-# print(pila._size)
+h2("replace_all:")
+pila.replace_all(17,4)
+print(pila)
+print(pila._size)
 
+h2("Exchange")
 pila.exchange()
 
 print(pila)
